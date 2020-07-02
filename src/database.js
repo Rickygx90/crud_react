016996ad04baf853e4144608bd17f94815452f33
Rickygx90 <rickygx.90@gmail.com>
2020-07-01 22:32:21 -0500

    Se instala dotenv, se agregan variables globales y gitignore

--- src/database.js
+++ src/database.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
-const URI = "mongodb+srv://crud_react:crud_react@cluster0.nfuw9.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
-mongoose.connect(URI)
+//Importar variables de entorno locales
+require('dotenv').config({path: 'variables.env'});
+console.log(process.env.URI);
+
+//const URI = "mongodb+srv://crud_react:crud_react@cluster0.nfuw9.mongodb.net/Cluster0?retryWrites=true&w=majority";
+//mongodb://localhost/mern-tasks
+//mongodb+srv://crud_react:crud_react@cluster0.nfuw9.mongodb.net/Cluster0?retryWrites=true&w=majority
+mongoose.connect(process.env.URI)
     .then(db => console.log('DB is connected'))
     .catch(err => console.log(err));
 
